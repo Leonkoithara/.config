9fb61d5819148341fb663d7b79ee6d623b6e4660
Leon Joe <36569141+Leonkoithara@users.noreply.github.com>
2020-07-07 14:10:14 +0530

    Add files via upload

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,16 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
+
+(load-theme 'deeper-blue)
+ (setq initial-frame-alist
+       '((top . 1) (left . 1) (width . 140) (height . 60)))
+
 
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
@@ -12,7 +19,7 @@
 (setq-default fill-column 85)
 
 (setq backup-by-copying t)
-(setq backup-directory-alist '(("." . "~/.emacs/backups")))
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 (setq delete-old-versions t)
 (setq kept-new-versions 3)
 (setq kept-old-versions 2)
@@ -21,15 +28,27 @@
 (setq c-default-style "bsd"
       c-basic-offset 4)
 
+(add-hook 'after-init-hook 'global-company-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org))))
+ '(package-selected-packages (quote (company-go company go-eldoc go-mode org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(defun go-mode-setup()
+  (linum-mode 1)
+  (go-eldoc-setup)
+  (setq tab-width 4)
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  )
+
+(add-hook 'go-mode-hook 'go-mode-setup)
