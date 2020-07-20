31842ec29ee7a4cb202108e0365f04c042a42a22
Leonkoithara <leon.koithara@gmail.com>
2020-07-20 23:57:27 +0530

    emacs changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,13 +8,16 @@
 (package-initialize)
 
 (load-theme 'deeper-blue)
- (setq initial-frame-alist
-       '((top . 1) (left . 1) (width . 140) (height . 60)))
+(add-hook 'before-make-frame-hook
+          #'(lambda ()
+              (add-to-list 'default-frame-alist '(left   . 1))
+              (add-to-list 'default-frame-alist '(top    . 1))
+              (add-to-list 'default-frame-alist '(height . 60))
+              (add-to-list 'default-frame-alist '(width  . 140))))
 
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
 
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c c") 'org-capture)
 
 (setq-default fill-column 85)
 
@@ -30,18 +33,31 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+(add-hook 'go-mode-hook 'go-mode-setup)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company-go company go-eldoc go-mode org))))
+ '(package-selected-packages
+   (quote
+    (company-irony irony company-go company go-eldoc go-mode org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
 
 (defun go-mode-setup()
   (linum-mode 1)
@@ -50,5 +66,3 @@
   (local-set-key (kbd "M-.") 'godef-jump)
   (add-hook 'before-save-hook 'gofmt-before-save)
   )
-
-(add-hook 'go-mode-hook 'go-mode-setup)
--- bash/.bashrc
+++ bash/.bashrc
@@ -5,6 +5,18 @@
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
 HISTSIZE=-1
 HISTFILESIZE=-1
 HISTCONTROL=ignoreboth
